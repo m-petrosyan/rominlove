7e4714f5838710e23b1c03e529fc28671cfda48a
Miqayel <tech_dev@amlincubator.com>
2025-02-20 19:25:09 +0400

    add meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,10 @@
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon.png]') }}"/>
+    <meta name="description" content="{{  'Default Description' }}">
+    <meta property="og:title" content="{{ 'Default Title' }}">
+    <meta property="og:description" content="{{  'Default Description' }}">
+    <meta property="og:image" content="{{ asset('favicon.png') }}">
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
